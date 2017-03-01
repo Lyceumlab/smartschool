228a18dacf4922ecf52ceb5b0c8f0466436f02ac
Thomas Egbert Duursma <thomasduursma@outlook.com>
2017-03-01 18:38:28 +0100

    Create smartschool.py
    Dit bestand kan een raspberry pi gebruiken om te communiceren met de server.
    ip is het ip adres van de server.
    id is het id van de sensor.
    value is de waarde die de sensor moet aangeven op de server.

---
+++ python/smartschool.py
@@ -0,0 +1,15 @@
+import urllib.request
+import json
+import sys
+
+ip = "localhost";
+id = 1;
+
+def addReading(id, value):
+    response = urllib.request.urlopen("http://" + ip + "/smartschool/addReading.php?id=" + str(id) + "&value=" + str(value)).read();
+    return true;
+
+def getReading(id):
+    response = urllib.request.urlopen("http://" + ip + "/smartschool/getReading.php?id=" + str(id)).read();
+    data = json.loads(response);
+    return data.value;
