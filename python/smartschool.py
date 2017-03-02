4001a24f25e98a5e5bb7ab86832b4974cf749e58
Thomas Egbert Duursma <thomasduursma@outlook.com>
2017-03-02 20:58:00 +0100

    Update smartschool.py

--- python/smartschool.py
+++ python/smartschool.py
@@ -2,13 +2,11 @@ import urllib.request
 import json
 import sys
 
-ip = "localhost";
-
 def addReading(id, value):
-    response = urllib.request.urlopen("http://" + ip + "/smartschool/addReading.php?id=" + str(id) + "&value=" + str(value)).read();
+    response = urllib.request.urlopen("http://" + ip + "/addReading.php?id=" + str(id) + "&value=" + str(value)).read();
     return True;
 
 def getReading(id):
-    response = urllib.request.urlopen("http://" + ip + "/smartschool/getReading.php?id=" + str(id)).read();
+    response = urllib.request.urlopen("http://" + ip + "/getReading.php?id=" + str(id)).read();
     data = json.loads(response);
     return data['value'];
