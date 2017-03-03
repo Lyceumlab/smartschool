723701cc8557d2cfefb1224f4c5ec4c5ddc66b23
Thomas Egbert Duursma <thomasduursma@outlook.com>
2017-03-03 19:53:20 +0100

    Update reading.js
    I accidentally pasted the contents of sensor.js into this file, sorry!

--- javascript/reading.js
+++ javascript/reading.js
@@ -1,84 +1,35 @@
 var SmSc = SmSc || {};
 
-SmSc.Sensor = class Sensor{
-    /*
-    * Create a sensor instance
-    * @param {number} id The id of the sensor, null if not in database
-    * @param {string} location The location of the sensor
-    * @param {string} type The type of the sensor
-    */
-    constructor(id, location, type) {
-        this.id = id;
-        this.location = location;
-        this.type = type;
-        this.readings = [];
+SmSc.Reading = class Reading {
+    constructor(sensor, value, dateTime) {
+        this.sensor = sensor;
+        this.value = value || 0;
+        this.dateTime = dateTime || new Date().getTime();
     }
 
-    /*
-    * Add the sensor to the database and set the id of the sensor
-    * @param {function} callback The callback, will be executed when done
-    */
     insert(callback) {
-        var sensor = this;
+        var reading = this;
+
         callback = callback || function(){};
-        $.ajax(SmSc.url + "/addSensor.php", {
+        $.ajax(SmSc.url + "/addReading.php", {
             method:"GET",
             dataType:"JSON",
-            data:SmSc.addCredentials({
-                location:this.location,
-                type:this.type
-            }),
+            data:{
+                id:this.sensor.id,
+                value:this.value
+            },
             success:function(response) {
-                var nSensor = response.sensor || {};
-                sensor.id = nSensor.id;
+                var nReading = response.reading;
+                reading.dateTime = nReading ? nReading.datetime : reading.dateTime;
 
                 var err = response.err || null;
 
-                if(response.err) callback(null, err);
-                else callback(sensor, err);
+                callback(reading, err);
             },
             error:function() {
                 console.log(arguments);
-                callback(sensor, "the http request failed");
+                callback(reading, "the http request failed");
             }
-        });
+        })
     }
-
-    get reading() {
-        return _.last(_.sortBy(this.readings, "dateTime"));
-    }
-
-    set reading(value) {
-        if(typeof value == "number") {
-            var reading = new SmSc.Reading(this, value, 0);
-            reading.insert();
-            this.readings.push(reading);
-        } else {
-            var reading = value;
-            reading.insert();
-            this.readings.push(reading);
-        }
-    }
-}
-
-SmSc.getSensors = function getSensors(callback) {
-    $.ajax(SmSc.url + "/getSensors.php", {
-        method:"GET",
-        dataType:"JSON",
-        data:SmSc.credentials,
-        success:function(response) {
-            var sensors = [];
-            for(var x in response.sensors) {
-                var object = response.sensors[x].sensor;
-                var sensor = new SmSc.Sensor(object.id, object.location, object.type);
-                for(var y in response.sensors[x].readings){
-                    var object2 = response.sensors[x].readings[y];
-                    var reading = new SmSc.Reading(sensor, object2.value, object2.dateTime);
-                    sensor.readings.push(reading);
-                }
-                sensors.push(sensor);
-            }
-            callback(sensors);
-        }
-    })
 }
